ef58afda223ed2609d247da893ffa42b53d7ba8e
dkoval <koval.dmitriy@gmail.com>
2022-12-01 09:43:34 +0100

    Solutions to Day01 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        var best = 0
+        for (s in input) {
+            if (s.isNotEmpty()) {
+                sum += s.toInt()
+                best = maxOf(best, sum)
+            } else {
+                sum = 0
+            }
+        }
+        return best
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        // keep top k integers
+        val k = 3
+        val minHeap = PriorityQueue<Int>()
+
+        fun enqueue(x: Int) {
+            minHeap.offer(x)
+            if (minHeap.size > k) {
+                minHeap.poll()
+            }
+        }
+
+        var sum = 0
+        for (s in input) {
+            if (s.isNotEmpty()) {
+                sum += s.toInt()
+            } else {
+                enqueue(sum)
+                sum = 0
+            }
+        }
+
+        enqueue(sum)
+        return minHeap.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
