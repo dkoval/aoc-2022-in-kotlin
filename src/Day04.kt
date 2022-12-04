9a38d1cb9bb2c797be7dcdc17edad8e8f111475d
dkoval <koval.dmitriy@gmail.com>
2022-12-04 10:17:54 +0100

    Solutions to Day04 challenge

---
+++ src/Day04.kt
@@ -0,0 +1,35 @@
+fun main() {
+    fun parseInput(input: List<String>): List<Pair<IntRange, IntRange>> =
+        input.map { line ->
+                val (first, second) = line.split(",").map { range ->
+                    val (start, end) = range.split("-")
+                    start.toInt()..end.toInt()
+                }
+                first to second
+            }
+
+    fun part1(input: List<String>): Int {
+        fun IntRange.fullyContains(b: IntRange): Boolean =
+            b.first >= this.first && b.last <= this.last
+
+        val data = parseInput(input)
+        return data.count { (first, second) -> first.fullyContains(second) || second.fullyContains(first) }
+    }
+
+    fun part2(input: List<String>): Int {
+        fun overlap(a: IntRange, b: IntRange): Boolean =
+            b.last >= a.first && b.first <= a.last
+
+        val data = parseInput(input)
+        return data.count { (first, second) -> overlap(first, second) }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+    println(part1(input)) // answer = 588
+    println(part2(input)) // answer = 911
+}
