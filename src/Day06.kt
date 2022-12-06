b29e9c8fe2d181c22384d28f935377b7050cb21c
dkoval <koval.dmitriy@gmail.com>
2022-12-06 08:37:30 +0100

    Solutions to Day06 challenge

---
+++ src/Day06.kt
@@ -0,0 +1,45 @@
+fun main() {
+    fun solve(input: String, k: Int): Int {
+        // sliding window [start : start + k - 1]
+        var start = 0
+        val seen = mutableMapOf<Char, Int>()
+        while (start + k < input.length) {
+            // starting from `start` index, check next k characters
+            for (i in start until start + k) {
+                if (input[i] in seen) {
+                    start = seen[input[i]]!! + 1
+                    seen.clear()
+                    break
+                }
+                seen[input[i]] = i
+            }
+
+            // are checked k characters unique?
+            if (seen.size == k) {
+                return start + k
+            }
+        }
+        return -1
+    }
+
+    fun part1(input: String): Int = solve(input, 4)
+
+    fun part2(input: String): Int = solve(input, 14)
+
+    // test if implementation meets criteria from the description, like:
+    check(part1(readInputAsString("Day06_test_part1_1")) == 7)
+    check(part1(readInputAsString("Day06_test_part1_2")) == 5)
+    check(part1(readInputAsString("Day06_test_part1_3")) == 6)
+    check(part1(readInputAsString("Day06_test_part1_4")) == 10)
+    check(part1(readInputAsString("Day06_test_part1_5")) == 11)
+
+    check(part2(readInputAsString("Day06_test_part2_1")) == 19)
+    check(part2(readInputAsString("Day06_test_part2_2")) == 23)
+    check(part2(readInputAsString("Day06_test_part2_3")) == 23)
+    check(part2(readInputAsString("Day06_test_part2_4")) == 29)
+    check(part2(readInputAsString("Day06_test_part2_5")) == 26)
+
+    val input = readInputAsString("Day06")
+    println(part1(input)) // answer = 1766
+    println(part2(input)) // answer = 2383
+}
