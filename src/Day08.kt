298afe55cd9941ab2982274d4f4c0599429d420c
dkoval <koval.dmitriy@gmail.com>
2022-12-08 19:16:21 +0100

    Further code readability improvements

--- src/Day08.kt
+++ src/Day08.kt
@@ -16,16 +16,16 @@ fun main() {
             // scan i-th row in both directions
             last[0] = grid[row][0]
             last[1] = grid[row][n - 1]
-            for (leftCol in 1 until n - 1) {
-                if (grid[row][leftCol] > last[0]) {
-                    last[0] = grid[row][leftCol]
-                    visible += row to leftCol
+            for (left in 1 until n - 1) {
+                if (grid[row][left] > last[0]) {
+                    last[0] = grid[row][left]
+                    visible += row to left
                 }
 
-                val rightCol = n - leftCol - 1
-                if (grid[row][rightCol] > last[1]) {
-                    last[1] = grid[row][rightCol]
-                    visible += row to rightCol
+                val right = n - left - 1
+                if (grid[row][right] > last[1]) {
+                    last[1] = grid[row][right]
+                    visible += row to right
                 }
             }
         }
@@ -35,16 +35,16 @@ fun main() {
             // scan i-th column in both directions
             last[0] = grid[0][col]
             last[1] = grid[m - 1][col]
-            for (topRow in 1 until m - 1) {
-                if (grid[topRow][col] > last[0]) {
-                    last[0] = grid[topRow][col]
-                    visible += topRow to col
+            for (top in 1 until m - 1) {
+                if (grid[top][col] > last[0]) {
+                    last[0] = grid[top][col]
+                    visible += top to col
                 }
 
-                val bottomRow = m - topRow - 1
-                if (grid[bottomRow][col] > last[1]) {
-                    last[1] = grid[bottomRow][col]
-                    visible += bottomRow to col
+                val bottom = m - top - 1
+                if (grid[bottom][col] > last[1]) {
+                    last[1] = grid[bottom][col]
+                    visible += bottom to col
                 }
             }
         }
@@ -66,11 +66,14 @@ fun main() {
         val n = grid[0].size
 
         fun viewingDistance(row: Int, col: Int, dx: Int, dy: Int): Int {
-            var dist = 1
+            var dist = 0
             var nextRow = row + dx
             var nextCol = col + dy
-            while (nextRow in 1 until m - 1 && nextCol in 1 until n - 1 && grid[nextRow][nextCol] < grid[row][col]) {
+            while (nextRow in 0 until m && nextCol in 0 until n) {
                 dist++
+                if (grid[nextRow][nextCol] >= grid[row][col]) {
+                    break
+                }
                 nextRow += dx
                 nextCol += dy
             }
