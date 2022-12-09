d582c547ffb6d8ce0c5a2be5f78007d051f522d4
dkoval <koval.dmitriy@gmail.com>
2022-12-09 10:19:38 +0100

    Solutions to Day09 challenge

---
+++ src/Day09.kt
@@ -0,0 +1,81 @@
+import kotlin.math.abs
+
+private const val DAY_ID = "09"
+
+private enum class Direction(val dx: Int, val dy: Int) {
+    L(0, -1), R(0, 1), U(1, 0), D(-1, 0),
+    UL(1, -1), UR(1, 1), DR(-1, 1), DL(-1, -1)
+}
+
+private data class Cell(
+    var row: Int,
+    var col: Int
+) {
+    fun move(d: Direction) {
+        row += d.dx
+        col += d.dy
+    }
+
+    fun isAdjacent(that: Cell): Boolean =
+        abs(row - that.row) <= 1 && abs(col - that.col) <= 1
+}
+
+fun main() {
+    fun move(knots: Array<Cell>, d: Direction, steps: Int, visited: MutableSet<Pair<Int, Int>>) {
+        val n = knots.size
+        repeat(steps) {
+            // move head
+            knots[0].move(d)
+
+            // move remaining knots, if needed
+            for (i in 1 until n) {
+                val head = knots[i - 1]
+                val tail = knots[i]
+                if (tail.isAdjacent(head)) continue
+
+                if (tail.row == head.row) {
+                    // head and tail are in the same row
+                    tail.move(if (head.col > tail.col) Direction.R else Direction.L)
+                } else if (tail.col == head.col) {
+                    // head and tail are in the same column
+                    tail.move(if (head.row > tail.row) Direction.U else Direction.D)
+                } else {
+                    // head and tail aren't in the same row or column,
+                    // therefore move tail one step diagonally to keep up
+                    if (head.row > tail.row) {
+                        tail.move(if (tail.col < head.col) Direction.UR else Direction.UL)
+                    } else {
+                        tail.move(if (tail.col < head.col) Direction.DR else Direction.DL)
+                    }
+                }
+            }
+            visited += knots[n - 1].row to knots[n - 1].col
+        }
+    }
+
+    fun solve(input: List<String>, n: Int): Int {
+        val data = input.map { line ->
+            val (d, steps) = line.split(" ")
+            enumValueOf<Direction>(d) to steps.toInt()
+        }
+
+        val knots = Array(n) { Cell(0, 0) }
+        val visited = mutableSetOf<Pair<Int, Int>>().also { it += 0 to 0 }
+        data.forEach { (d, steps) -> move(knots, d, steps, visited) }
+        return visited.size
+    }
+
+    fun part1(input: List<String>): Int = solve(input, 2)
+
+    fun part2(input: List<String>): Int = solve(input, 10)
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${DAY_ID}_test")
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 1)
+    check(part2(readInput("Day${DAY_ID}_test_part2")) == 36)
+
+    val input = readInput("Day${DAY_ID}")
+    println(part1(input)) // answer = 6037
+    println(part2(input)) // answer = 2485
+}
