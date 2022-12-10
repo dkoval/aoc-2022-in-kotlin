fdfb5d6ae8fabc1fac3a5cecfa80026888c0691d
dkoval <koval.dmitriy@gmail.com>
2022-12-10 19:31:57 +0100

    Generalize Part1 and Part2 solutions of Day10 challenge

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,31 +1,43 @@
 private const val DAY_ID = "10"
 
 fun main() {
+    fun solve(input: List<String>, onCycle: (cycle: Int, x: Int) -> Boolean) {
+        var x = 1
+        var cycle = 1
+        for (line in input) {
+            // noop
+            var delta = 0
+            var cycleToComplete = 1
+
+            // addx <delta>
+            if (line.startsWith("addx")) {
+                delta = line.removePrefix("addx ").toInt()
+                cycleToComplete = 2
+            }
+
+            // execute instruction
+            repeat(cycleToComplete) {
+                if (onCycle(cycle++, x)) {
+                    // early termination
+                    return
+                }
+            }
+
+            // finish execution by updating the state of the X register (only relevant for `addx`)
+            x += delta
+        }
+    }
+
     fun part1(input: List<String>): Int {
         val cycles = setOf(20, 60, 100, 140, 180, 220)
-
-        var x = 1
         val signalStrengths = mutableListOf<Int>()
-        fun checkCycle(cycle: Int) {
+
+        solve(input) { cycle, x ->
             if (cycle in cycles) {
                 signalStrengths += x * cycle
             }
-        }
-
-        var i = 1
-        for (line in input) {
-            if (line.startsWith("addx")) {
-                // addx <delta>
-                val delta = line.removePrefix("addx ").toInt()
-                // "start" and "during" phases of an "addx" instruction do not modify x
-                repeat(2) {
-                    checkCycle(i++)
-                }
-                x += delta
-            } else {
-                // noop
-                checkCycle(i++)
-            }
+            // condition for early termination
+            signalStrengths.size == cycles.size
         }
         return signalStrengths.sum()
     }
@@ -35,31 +47,16 @@ fun main() {
         val height = 6
         val screen = Array(height) { CharArray(width) { '.' } }
 
-        var x = 1 // middle of the 3 pixels wide sprite
-        fun printPixel(i: Int) {
-            val row = i / width
-            val col = i % width
+        solve(input) { cycle, x ->
+            val row = (cycle - 1) / width
+            val col = (cycle - 1) % width
             // if the sprite is positioned such that one of its three pixels is the pixel currently being drawn,
             // the screen produces a lit pixel
             if (col in (x - 1)..(x + 1)) {
                 screen[row][col] = '@'
             }
-        }
-
-        var i = 0
-        input.forEach { line ->
-            if (line.startsWith("addx")) {
-                // addx <delta>
-                val delta = line.removePrefix("addx ").toInt()
-                // "start" and "during" phases of an "addx" instruction do not modify x
-                repeat(2) {
-                    printPixel(i++)
-                }
-                x += delta
-            } else {
-                // noop
-                printPixel(i++)
-            }
+            // no condition for early termination, process the input till the end
+            false
         }
         return screen.joinToString(separator = "\n") { it.joinToString(separator = " ") }
     }
