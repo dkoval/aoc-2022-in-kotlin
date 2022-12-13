8663c49e073975c9bc55e5b4030842df3bdf61fd
dkoval <koval.dmitriy@gmail.com>
2022-12-13 16:51:32 +0100

    Generalize sorting and comparison logic by making PacketItem a Comparable<PacketItem>

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,11 +1,48 @@
 private const val DAY_ID = "13"
 
-private sealed class PacketItem {
+private sealed class PacketItem: Comparable<PacketItem> {
     data class Num(val x: Int) : PacketItem() {
         fun toSeq(): Seq = Seq(listOf(this))
     }
 
     data class Seq(val items: List<PacketItem>) : PacketItem()
+
+    override fun compareTo(other: PacketItem): Int {
+        // base case #1
+        if (this is Num && other is Num) {
+            return compareValues(this.x, other.x)
+        }
+
+        // base case #2
+        if (this is Seq && other is Seq) {
+            var i = 0
+            while (i < minOf(this.items.size, other.items.size)) {
+                val res = this.items[i].compareTo(other.items[i])
+                if (res != 0) {
+                    return res
+                }
+                i++
+            }
+            return when {
+                // check #1 - lists are of the same length and no comparison makes a decision about the order
+                this.items.size == other.items.size -> 0
+                // check #2 - the left list runs out of items first, the inputs are in the right order
+                i == this.items.size -> -1
+                // check #3 - the right list runs out of items first, the inputs are not in the right order
+                else -> 1
+            }
+        }
+
+        // if exactly one value is an integer, convert it to a list, then retry the comparison
+        if (this is Num) {
+            return this.toSeq().compareTo(other)
+        }
+
+        if (other is Num) {
+            return this.compareTo(other.toSeq())
+        }
+        return 0
+    }
 
     companion object {
         fun fromString(s: String): PacketItem {
@@ -40,43 +77,6 @@ private sealed class PacketItem {
             }
             return Seq(traverse(mutableListOf()))
         }
-
-        fun compare(left: PacketItem, right: PacketItem): Int {
-            // base case #1
-            if (left is Num && right is Num) {
-                return compareValues(left.x, right.x)
-            }
-
-            // base case #2
-            if (left is Seq && right is Seq) {
-                var i = 0
-                while (i < minOf(left.items.size, right.items.size)) {
-                    val res = compare(left.items[i], right.items[i])
-                    if (res != 0) {
-                        return res
-                    }
-                    i++
-                }
-                return when {
-                    // check #1 - lists are of the same length and no comparison makes a decision about the order
-                    left.items.size == right.items.size -> 0
-                    // check #2 - the left list runs out of items first, the inputs are in the right order
-                    i == left.items.size -> -1
-                    // check #3 - the right list runs out of items first, the inputs are not in the right order
-                    else -> 1
-                }
-            }
-
-            // if exactly one value is an integer, convert it to a list, then retry the comparison
-            if (left is Num) {
-                return compare(left.toSeq(), right)
-            }
-
-            if (right is Num) {
-                return compare(left, right.toSeq())
-            }
-            return 0
-        }
     }
 }
 
@@ -91,27 +91,26 @@ fun main() {
         val pairs = parseInput(input)
         val sum = pairs.foldIndexed(0) { index, acc, (left, right) ->
             // sum indices (1-indexed) of the pairs that are in the right order
-            acc + if (PacketItem.compare(left, right) < 0) index + 1 else 0
+            acc + if (compareValues(left, right) < 0) index + 1 else 0
         }
         return sum
     }
 
     fun part2(input: String): Int {
-        val dividers = sequenceOf("[[2]]", "[[6]]").map { PacketItem.fromString(it) }.toList()
+        val dividers = sequenceOf("[[2]]", "[[6]]")
+            .map { PacketItem.fromString(it) }
+            .toSortedSet()
 
         val pairs = parseInput(input)
         val packets = dividers.toMutableList()
         packets += pairs.flatMap { it.toList() }
 
         // put the packets in the correct order
-        packets.sortWith { left, right -> PacketItem.compare(left, right) }
+        packets.sort()
 
         // find the decoder key
-        val key = dividers.fold(1) { acc, divider ->
-            val (index, _) = packets.withIndex()
-                .find { (_, item) -> PacketItem.compare(item, divider) == 0 }
-                ?: error("Not found: $divider")
-            acc * (index + 1)
+        val key = packets.foldIndexed(1) { index, acc, item ->
+            acc * if (item in dividers) index + 1 else 1
         }
         return key
     }
