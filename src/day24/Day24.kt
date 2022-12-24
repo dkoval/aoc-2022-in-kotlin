2c31e78be7ab6b2d8a896a3dd15bcc6f5c7b43b1
dkoval <koval.dmitriy@gmail.com>
2022-12-25 00:35:09 +0100

    Solution to Part 2 of Day24 challenge

--- src/day24/Day24.kt
+++ src/day24/Day24.kt
@@ -12,8 +12,7 @@ private enum class Direction(
     UP(-1, 0),
     DOWN(1, 0),
     LEFT(0, -1),
-    RIGHT(0, 1),
-    NOOP(0, 0);
+    RIGHT(0, 1);
 
     companion object {
         fun fromString(s: String): Direction = when (s) {
@@ -30,7 +29,7 @@ private data class Position(
     val row: Int,
     val col: Int
 ) {
-    fun isInsideGrid(numRows: Int, numCols: Int): Boolean = (row in 0 until numRows) && (col in 0 until numCols)
+    fun isInBounds(numRows: Int, numCols: Int): Boolean = (row in 0 until numRows) && (col in 0 until numCols)
 
     fun next(direction: Direction): Position = Position(row + direction.dx, col + direction.dy)
 
@@ -54,8 +53,8 @@ private data class Blizzard(
 )
 
 private data class Grid(
-    val source: Position,
-    val target: Position,
+    val start: Position,
+    val goal: Position,
     val blizzards: Set<Blizzard>,
     val numRows: Int,
     val numCols: Int
@@ -109,27 +108,27 @@ fun main() {
             return Direction.values().any { Blizzard(position, it) in blizzards }
         }
 
-        fun solve(source: Position, target: Position, time: Int): Int {
-            // Dijkstra's algorithm
-            val q: Queue<TimedPosition> = PriorityQueue(compareBy { it.time })
+        fun solve(start: Position, goal: Position, time: Int): Int {
+            // BFS
+            val q: Queue<TimedPosition> = ArrayDeque()
             val visited = mutableSetOf<TimedPosition>()
-            fun enqueue(position: TimedPosition) {
-                q.offer(position)
-                visited += position
+            fun enqueue(item: TimedPosition) {
+                q.offer(item)
+                visited += item
             }
 
-            enqueue(TimedPosition(time, source))
+            enqueue(TimedPosition(time, start))
             while (!q.isEmpty()) {
                 val curr = q.poll()
 
-                // move in each possible direction or wait in place
+                // move in each possible direction
                 for (direction in Direction.values()) {
                     val next = curr.next(direction)
-                    if (next.position == target) {
+                    if (next.position == goal) {
                         return next.time
                     }
 
-                    if (!next.position.isInsideGrid(grid.numRows, grid.numCols) || next in visited) {
+                    if (!next.position.isInBounds(grid.numRows, grid.numCols) || next in visited) {
                         continue
                     }
 
@@ -137,23 +136,111 @@ fun main() {
                         enqueue(next)
                     }
                 }
+
+                // wait in place
+                if (!occupiedByBlizzards(curr.position, curr.time + 1)) {
+                    enqueue(TimedPosition(curr.time + 1, curr.position))
+                }
             }
-            error("Couldn't find the shortest path from $source to $target starting at time $time")
+            error("Couldn't find the shortest path from $start to $goal")
         }
 
-        return solve(grid.source, grid.target, 0)
+        return solve(grid.start, grid.goal, 0)
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val grid = parseInput(input)
+
+        fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+        fun lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
+
+        // generate all possible states blizzards can be in
+        val period = lcm(grid.numRows, grid.numCols)
+        val states = mutableListOf(grid.blizzards)
+        for (i in 1 until period) {
+            val prev = states[i - 1]
+            val curr = mutableSetOf<Blizzard>()
+            // move blizzards
+            for ((position, direction) in prev) {
+                val newPosition = position.nextCircular(direction, grid.numRows, grid.numCols)
+                curr += Blizzard(newPosition, direction)
+            }
+            states += curr
+        }
+
+        fun occupiedByBlizzards(position: Position, time: Int): Boolean {
+            val blizzards = states[time % period]
+            return Direction.values().any { Blizzard(position, it) in blizzards }
+        }
+
+        fun solve(start: Position, goal: Position): Int {
+            // BFS
+            val q: Queue<Pair<TimedPosition, Int>> = ArrayDeque()
+            val visited = mutableSetOf<Pair<TimedPosition, Int>>()
+            fun enqueue(item: Pair<TimedPosition, Int>) {
+                q.offer(item)
+                visited += item
+            }
+
+            // extra `pass` argument in the state:
+            // 0 - 1st trip from start to goal
+            // 1 - trip back from goal to start
+            // 2 - trip back from start to goal again
+            enqueue(TimedPosition(0, start) to 0)
+            while (!q.isEmpty()) {
+                val (curr, pass) = q.poll()
+
+                // move in each possible direction
+                for (direction in Direction.values()) {
+                    val next = curr.next(direction)
+                    when {
+                        next.position == goal && (pass == 0 || pass == 2) -> {
+                            if (pass == 0) {
+                                // now trip back to the start
+                                q.offer(next to 1)
+                            } else {
+                                return next.time
+                            }
+                        }
+
+                        next.position == start && pass == 1 -> {
+                            // trip back to the goal again
+                            q.offer(next to 2)
+                            break
+                        }
+
+                        else -> {
+                            if (!next.position.isInBounds(grid.numRows, grid.numCols)
+                                || occupiedByBlizzards(next.position, next.time)
+                            ) {
+                                continue
+                            }
+
+                            val nextState = next to pass
+                            if (nextState !in visited) {
+                                enqueue(nextState)
+                            }
+                        }
+                    }
+                }
+
+                // wait in place
+                if (!occupiedByBlizzards(curr.position, curr.time + 1)) {
+                    enqueue(TimedPosition(curr.time + 1, curr.position) to pass)
+                }
+            }
+            error("Couldn't find the shortest path from $start to $goal")
+        }
+
+        return solve(grid.start, grid.goal)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day${DAY_ID}/Day${DAY_ID}_test")
     check(part1(testInput) == 18)
-    //check(part2(testInput) == 54)
+    check(part2(testInput) == 54)
 
     val input = readInput("day${DAY_ID}/Day${DAY_ID}")
     println(part1(input)) // answer = 292
-    //println(part2(input))
+    println(part2(input)) // answer = 816
 }
