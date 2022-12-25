47f838761386f86801a071517502603fe6440cfc
dkoval <koval.dmitriy@gmail.com>
2022-12-25 12:32:52 +0100

    Code readability improvements

--- src/day25/Day25.kt
+++ src/day25/Day25.kt
@@ -5,31 +5,31 @@ import readInput
 private const val DAY_ID = "25"
 
 fun main() {
-    val snafuToBase10Digits = mapOf('=' to -2, '-' to -1, '0' to 0, '1' to 1, '2' to 2)
-    val base10ToSnafuDigits = mapOf(0 to '0', 1 to '1', 2 to '2', -2 to '=', -1 to '-')
+    val snafuToIntDigits = mapOf('=' to -2, '-' to -1, '0' to 0, '1' to 1, '2' to 2)
+    val intToSnafuDigits = mapOf(0 to '0', 1 to '1', 2 to '2', -2 to '=', -1 to '-')
 
-    fun snafuToBase10(num: String): Long {
+    fun snafuToInt(num: String): Long {
         var ans = 0L
         for (digit in num) {
             ans *= 5
-            ans += snafuToBase10Digits[digit]!!
+            ans += snafuToIntDigits[digit]!!
         }
         return ans
     }
 
-    fun base10ToSnafu(num: Long): String {
+    fun intToSnafu(num: Long): String {
         // x mod 5 is in {0, 1, 2, 3, 4}
         // now, remap the value to get a SNAFU digit
         // 0 -> 0, 1 -> 1, 2 -> 2, 3 -> -2, 4 -> -1
-        fun base10SnafuDigit(x: Long): Int {
+        fun snafuDigit(x: Long): Int {
             return (if (x < 3) x else x - 5).toInt()
         }
 
         var x = num
         val sb = StringBuilder()
         while (x > 0) {
-            val digit = base10SnafuDigit(x % 5)
-            sb.append(base10ToSnafuDigits[digit])
+            val digit = snafuDigit(x % 5)
+            sb.append(intToSnafuDigits[digit])
             x /= 5
             x += if (digit < 0) 1 else 0
         }
@@ -37,8 +37,8 @@ fun main() {
     }
 
     fun part1(input: List<String>): String {
-        val x = input.sumOf { snafuToBase10(it) }
-        return base10ToSnafu(x)
+        val x = input.sumOf { snafuToInt(it) }
+        return intToSnafu(x)
     }
 
     // test if implementation meets criteria from the description, like:
